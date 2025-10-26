f94269803baaaaf90ac2447eeadd8cf484b027f7
Ilyazbek2 <101692898+Ilyazbek2@users.noreply.github.com>
2025-10-26 20:54:58 +0100

    Create views_tasks.sql

---
+++ views_tasks.sql
@@ -0,0 +1,20 @@
+-- 1. Simple view with employee names
+CREATE OR REPLACE VIEW v_employees AS
+SELECT first_name, last_name
+FROM employees;
+
+-- 2. View joining employees and current salaries
+CREATE OR REPLACE VIEW v_emp_sal AS
+SELECT e.emp_no, e.first_name, e.last_name, s.salary
+FROM employees e
+JOIN salaries s ON e.emp_no = s.emp_no
+WHERE s.to_date = '9999-01-01';
+
+-- 3. View showing average salary by department
+CREATE OR REPLACE VIEW v_avg_salary AS
+SELECT d.dept_name, AVG(s.salary) AS avg_salary
+FROM salaries s
+JOIN dept_emp de ON s.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE s.to_date = '9999-01-01'
+GROUP BY d.dept_name;
